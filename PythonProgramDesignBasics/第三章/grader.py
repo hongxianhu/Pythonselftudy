e4db7d4a3ba309dcfae4f9879ae7adbd8f93c1bc
34797 <3479716187@QQ.com>
2026-01-26 20:23:55 +0800

    书v1-章节3-时间0126-代码

---
+++ PythonProgramDesignBasics/第三章/grader.py
@@ -0,0 +1,20 @@
+A_SCORE=90
+B_SCORE=80
+C_SCORE=70
+D_SCORE=60
+
+score=float(input('enter the test score: '))
+
+if score>=A_SCORE:
+    print('your grade is A.')
+else:
+    if score>=B_SCORE:
+        print('your grade is B.')
+    else:
+        if score>=C_SCORE:
+            print('your grade is C.')
+        else:
+            if score>=D_SCORE:
+                print('your grade is D.')
+            else:
+                print('your grade is F.')
